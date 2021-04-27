a89ea434c8e9009ac025b86312ed6d813f2d3847
Oli Fog <oliver@fogelin.com>
2021-04-27 21:45:46 +0100

    app proptypes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import PropTypes from 'prop-types'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
