b2e58a938c1cb6b8dd43bade397c470f1e0e1227
Oli Fog <oliver@fogelin.com>
2021-04-29 22:27:20 +0100

    I genuinely despise microsoft graph

--- components/Navbar/UserItem.js
+++ components/Navbar/UserItem.js
@@ -1,11 +1,38 @@
 import Image from 'next/image'
 import PropTypes from 'prop-types'
+import { Client } from '@microsoft/microsoft-graph-client'
+import { useState, useEffect } from 'react'
+
+import AuthenticationProvider from '../../lib/authProvider'
 
 export default function UserItem ({ user, logout }) {
+  const [photo, setPhoto] = useState(null)
+
+  useEffect(async () => {
+    const options = {
+      authProvider: new AuthenticationProvider(user)
+    }
+    const client = Client.initWithMiddleware(options)
+
+    await client.api('/me/photo/$value').get().then(u => {
+      const reader = new FileReader()
+      reader.readAsDataURL(u)
+      reader.onloadend = () => {
+        setPhoto(reader.result)
+      }
+    })
+  }, [])
+
+  if (photo === null) {
+    return (
+      <div>...</div>
+    )
+  }
+
   return (
     <div className="flex flex-row items-center pl-8">
       <Image
-        src={'https://dummyimage.com/40x40/000/fff'}
+        src={photo}
         width={40}
         height={40}
         className="rounded-full"
